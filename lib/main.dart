a35d105bd2444d812e96f6b6b12d4de9d18f6ae4
Chris Wong <wc38.wong@gmail.com>
2023-06-19 18:19:52 +0800

    Flutter Tutorial for Beginners #19 - Cards

--- lib/main.dart
+++ lib/main.dart
@@ -60,23 +60,26 @@ class QuoteListState extends State<QuoteList> {
   Widget quoteTemplate(quote) {
     return Card(
       margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Column(children: <Widget>[
-        Text(
-          quote.text,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Colors.grey[600],
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(children: <Widget>[
+          Text(
+            quote.text,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.grey[600],
+            ),
           ),
-        ),
-        const SizedBox(height: 6.0),
-        Text(
-          quote.author,
-          style: TextStyle(
-            fontSize: 14.0,
-            color: Colors.grey[800],
+          const SizedBox(height: 6.0),
+          Text(
+            quote.author,
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.grey[800],
+            ),
           ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 
