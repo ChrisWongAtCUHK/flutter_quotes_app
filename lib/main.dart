a0803a172ee0c8686e583e40eab933691b3bfae5
Chris Wong <wc38.wong@gmail.com>
2023-06-20 08:12:54 +0800

    Flutter Tutorial for Beginners #20 - Extracting Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -68,41 +69,6 @@ class QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
-      ),
-    );
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-  final Quote quote;
-
-  const QuoteCard({super.key, required this.quote});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                quote.text,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.grey[600],
-                ),
-              ),
-              const SizedBox(height: 6.0),
-              Text(
-                quote.author,
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey[800],
-                ),
-              ),
-            ]),
       ),
     );
   }
